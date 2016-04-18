c5d578fd3027f7b15ff226c93cc56d67537ded77
Sam Davis <sam@sjd.co>
2016-04-18 11:18:09 +1000

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: SJD Settings Page
+Plugin URI: http://sjd.co
+Description: Simple settings page for use on SJD.co
+Version: 0.1
+Author: Samuel Davis
+Author URI: http://sjd.co
+*/
+?>
+
